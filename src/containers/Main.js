ddb02baa55bcc461f45a2f416060d76a4a1b7de3
Manoj Walia <javadeveloper176097@gmail.com>
2022-08-21 13:31:20 +0900

    Changes

--- src/containers/Main.js
+++ src/containers/Main.js
@@ -114,19 +114,16 @@ export default class Main extends Component {
                   <Contact {...props} theme={this.props.theme} />
                 )}
               />
-              {/* <Route
-							path="/splash"
-							render={(props) => (
-								<Splash
-									{...props}
-									theme={this.props.theme}
-								/>
-							)}
-						/> */}
+              <Route
+                path="/splash"
+                render={(props) => (
+                  <Splash {...props} theme={this.props.theme} />
+                )}
+              />
               <Route
                 path="/projects"
                 render={(props) => (
-                  <Projects {...props} theme={this.props.theme} />
+                  <Error404 {...props} theme={this.props.theme} />
                 )}
               />
             </Switch>
